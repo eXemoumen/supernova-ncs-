14cddb6b2ef3e979902563ab9b276fc18f29efa2
AbrASM1 <abderrahimasmane@gmail.com>
2025-06-30 09:54:12 +0200

    update 2

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const db = require('./model/db');
 const authRoutes = require('./routes/auth')
+const accessRoutes = require('./routes/user_access')
 // const agentRoutes = require('./routes/agent')
 // const departmentRoutes = require('./routes/departments')
 // const exportRoutes = require('./routes/export')
@@ -13,6 +15,7 @@ app.use(express.json())
 
 // Routes
 app.use('/api/auth', authRoutes)
+app.use('/api/access', accessRoutes)
 // app.use('/api/departments', departmentRoutes)
 // app.use('/api/agent', agentRoutes)
 // app.use('/api/export', exportRoutes)
@@ -24,3 +27,14 @@ app.use('/test',(req,res)=>{
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`API running on ${PORT}`))
+async function testDBConnection() {
+    try {
+      const [rows] = await db.execute('SELECT 1');
+      console.log('✅ MySQL database connected!');
+    } catch (err) {
+      console.error('❌ Failed to connect to MySQL:', err.message);
+      process.exit(1); // stop the app if DB fails
+    }
+  }
+  
+  testDBConnection();
